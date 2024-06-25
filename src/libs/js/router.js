294a86248a565ac1dd2a40e835647f3cea6c0e8b
snowca-222 <holybell222@gmail.com>
2024-06-26 01:37:10 +0800

    修正1

--- src/libs/js/router.js
+++ src/libs/js/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
+import Home from '@/components/Home.vue'
 const router = createRouter({
     // history: createWebHistory('/TPN/'), // 這邊要把 vite.config.js 的 base 填入
     history: createWebHashHistory('/TPN/'), // 這邊要把 vite.config.js 的 base 填入
@@ -6,7 +7,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'Home',
-            component: () => import('@/components/Home.vue')
+            component: Home
         },
         {
             path: '/user',
