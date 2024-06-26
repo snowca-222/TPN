7e40c67a2aa06d790b8becb03af29032076b49e5
snowca-222 <holybell222@gmail.com>
2024-06-26 12:10:05 +0800

    新增user框架

--- src/components/template/userInfo/index.vue
+++ src/components/template/userInfo/index.vue
@@ -2,19 +2,37 @@
 <template>
   <div class="w-1/2 p-2.5">
     <div
-      class="rounded-md bg-[--bg-1] p-2.5 transition-colors hover:bg-[--bg-2]"
+      class="rounded-md bg-[--color-1] p-2.5 transition-colors hover:bg-[--color-2]"
     >
-      <div>頭像</div>
-      <div>姓名</div>
-      <div>
-        <div>職業</div>
-        <div>服務地區</div>
-        <div>Email</div>
+      <img
+        class="m-auto aspect-[1/1] w-[150px]"
+        src="@image/icons/userIcon.png"
+        alt="noPhotoUser"
+      />
+      <div class="text-white">
+        <div class="my-5 text-center text-xl font-bold">
+          Christine Juang, PhD, DBSM
+        </div>
+        <ul class="m-auto my-5 w-fit text-sm [&>li]:py-1">
+          <li>Licensed Psychologist</li>
+          <li>Missouri and California</li>
+          <li>Email Address</li>
+        </ul>
       </div>
-      <div class="flex">
-        <button class="w-1/2">Website</button>
+      <div class="flex [&>a]:w-full [&>a]:px-2.5">
+        <a href="https://www.google.com/" target="_blank">
+          <button
+            class="w-full rounded-md bg-[--color-6] py-2.5 text-sm text-[--color-7] hover:bg-[--color-3] hover:text-[--color-17]"
+          >
+            Website
+          </button>
+        </a>
         <router-link :to="{ name: 'User' }">
-          <button class="w-1/2">View Profile</button>
+          <button
+            class="w-full rounded-md bg-[--color-9] py-2.5 text-sm text-[--color-10] hover:bg-[--color-4] hover:text-[--color-5]"
+          >
+            View Profile
+          </button>
         </router-link>
       </div>
     </div>
