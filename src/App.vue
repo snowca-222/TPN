9b2dd36b59dd06fadb3490bd3c744feae900a0d4
snowca-222 <holybell222@gmail.com>
2024-07-07 02:49:43 +0800

    新增 go top 按鈕，修正 router 執行時跳到頂端

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
+import { ref } from "vue";
 import Header from "@/components/template/header/index.vue";
 import Footer from "@/components/template/footer/index.vue";
+import ScrollTop from "@/components/template/scrollTop/index.vue";
+const handleShow = ref(true);
 </script>
-
 <template>
   <div class="flex min-h-dvh flex-col">
     <Header />
     <router-view class="flex-1" />
     <Footer />
+    <ScrollTop v-show="handleShow" />
   </div>
 </template>
---
+++ src/components/template/scrollTop/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+const handleScrollTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+</script>
+<template>
+  <button
+    @click="handleScrollTop"
+    class="fixed bottom-20 right-10 flex aspect-[1/1] w-16 items-center justify-center rounded-full border-8 border-[--color-17] bg-white text-[--color-17] opacity-50 shadow-lg hover:opacity-100"
+  >
+    <div class="relative">
+      <div class="absolute left-1.5 h-1 w-3 -rotate-45 bg-[--color-17]"></div>
+      <div class="absolute right-1.5 h-1 w-3 rotate-45 bg-[--color-17]"></div>
+      <div class="mt-2">TOP</div>
+    </div>
+  </button>
+</template>
