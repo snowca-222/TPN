324c911b7e0f17f2d6d273267ba214992e7a8ae1
snowca-222 <holybell222@gmail.com>
2024-07-22 02:08:31 +0800

    先移除 nodata 頁面

--- src/components/template/userList/index.vue
+++ src/components/template/userList/index.vue
@@ -33,28 +33,28 @@ onMounted(() => {
       <UserInfoBoxSkeleton v-for="i in 3" :key="i" />
     </template>
     <template v-else>
-      <template v-if="memberSearchDatas.length > 0">
+      <template v-for="memberData in memberSearchDatas" :key="memberData.name">
+        <template v-if="countrySelect === 'USA' && memberData.activeInUSA">
+          <UserInfoBox :memberData="memberData" />
+        </template>
+
         <template
-          v-for="memberData in memberSearchDatas"
-          :key="memberData.name"
+          v-else-if="countrySelect === 'TW' && memberData.licensureTaiwan"
         >
-          <template v-if="countrySelect === 'USA' && memberData.activeInUSA">
-            <UserInfoBox :memberData="memberData" />
-          </template>
-          <template
-            v-else-if="countrySelect === 'TW' && memberData.licensureTaiwan"
-          >
-            <UserInfoBox :memberData="memberData" />
-          </template>
+          <UserInfoBox :memberData="memberData" />
         </template>
       </template>
-      <div v-else class="w-full px-2.5 py-5">
-        <img src="@image/icons/nodata.svg" alt="nodata" class="mx-auto w-60" />
+      <!-- <div v-else class="w-full px-2.5 py-5">
+        <img
+          src="@image/icons/nodata.svg"
+          alt="nodata"
+          class="mx-auto w-60"
+        />
         <div class="text-center text-2xl font-bold">Result Not Found</div>
         <p class="py-5 text-center text-gray-500">
           Oops...please try choosing a different filter setting.
         </p>
-      </div>
+      </div> -->
     </template>
   </div>
 </template>
